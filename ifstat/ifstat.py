2f1d8217b9aa46b6bdcc6f51ab977c65c0dff9c0
Aviram Cohen <aviram@adallom.com>
2015-08-27 20:12:52 +0300

    Added key handling

--- ifstat/ifstat.py
+++ ifstat/ifstat.py
@@ -1,11 +1,12 @@
 import argparse
 import locale
 import curses
-import time
 
 from .stat_processor import StatProcessor
 from .display.sessions import SessionsPad
 from . import getstats
+
+DEFAULT_INTERVAL = 10
 
 def main():
     arg_parser = argparse.ArgumentParser(description="ifstat - network interface statistics utility")
@@ -24,14 +25,22 @@ def main():
     window = curses.initscr()
     sessions_pad = SessionsPad()
     curses.noecho()
+    curses.cbreak()
+    curses.halfdelay(DEFAULT_INTERVAL)
     try:
-        while True:
-            time.sleep(args.interval)
+        running = True
+        while running:
             raw_stats = collector.get_stats()
             current_stats = stat_processor.process_new_stats(raw_stats, args.interval)
 
             sessions_pad.display(current_stats["sessions"])
+            key = window.getch()
 
+            if key == ord('q'):
+                collector.stop()
+                collector.join()
+                running = False
     finally:
+        curses.nocbreak()
         curses.echo()
         curses.endwin()
